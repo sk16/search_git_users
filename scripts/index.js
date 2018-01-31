70fd9503b8e4f5c60e26f10e974e949e08ff9a29
sk16 <sksauravkushwaha16@gmail.com>
2018-01-31 17:58:13 +0530

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  var xhr;
+  $("#users").keyup(function(){
+      var user = $('#users').val();
+      var url = 'https://api.github.com/users/'+user;
+      if(xhr){
+        xhr.abort();
+      }
+
+      xhr = $.ajax({
+        url: url,
+        type: 'GET', 
+        success: function(result){
+           renderResult(result);
+        },
+        error: function(xhr,status,error){
+           $('#result').text('User Not Found!!');
+           $('#user_data').hide();
+        }
+      });
+
+  });
+
+  function renderResult(res){
+     $('#result').text('User Found!!');
+     $('#display_photo')[0].src = res.avatar_url;
+     $('#nick_name').text(res.login);
+     $('#user_name').text(res.name);
+     $('#company').text(res.company);
+     $('#user_data').show();
+  }
+});
